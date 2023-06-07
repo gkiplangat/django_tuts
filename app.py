b62cb248b5e6bf2366d0b65ffe7ea8cefce5e20c
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-06-07 23:21:28 +0300

    This fixes #1

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ name = input("Hello, Iam app.py, what is your name? ")
 age = int(input("Alright, pleasure to know you, how old are you name"))
 
 #print out the inputs
-print("Hello {} you are {} years old".format(name, age))
+print("Hello {}  now you are {} years old".format(name, age))
