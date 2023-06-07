e4f47bb189930b4006a003e6cc728fc2e9aad030
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-06-07 23:28:16 +0300

    doc: This fixes #2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 name = input("Hello, Iam app.py, what is your name? ")
 age = int(input("Alright, pleasure to know you, how old are you name"))
+gender = input("Your gender:")
 
 #print out the inputs
 print("Hello {}  now you are {} years old".format(name, age))
