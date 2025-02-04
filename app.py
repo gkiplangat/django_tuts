1c1bf57354a365c70db8d7978e7e58ba3fe3986e
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2025-02-04 17:13:51 +0300

    deleted file

--- app.py
+++
@@ -1,6 +0,0 @@
-name = input("Hello, Iam app.py, what is your name? ")
-age = int(input("Alright, pleasure to know you, how old are you name"))
-gender = input("Your gender:")
-
-#print out the inputs
-print("Hello {} you are {} aged {} years old".format(name,gender, age))
